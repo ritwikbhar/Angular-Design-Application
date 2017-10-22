00725e380d5c56542f2b86c8d3d8433ade6b3369
Ritwik Bhar <ritwikbhar21@gmail.com>
2017-10-22 22:04:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {    
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: "players",
+        component: PlayersComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
